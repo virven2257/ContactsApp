9841e0c52b5058d675eaef111c99a917db6b3695
virven <virven2257@gmail.com>
2020-10-12 23:40:53 +0700

    new_form added

---
+++ ContactsAppUI/new_form.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ContactsAppUI
+{
+    public partial class new_form : Form
+    {
+        public new_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
