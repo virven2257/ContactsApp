ee423d28c8a5a4229f3da1c0e19e66cb15c9af4d
virven <virven2257@gmail.com>
2021-02-04 22:00:48 +0700

    Начало работы над основной формой

---
+++ ContactsAppUI/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ContactsAppUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
